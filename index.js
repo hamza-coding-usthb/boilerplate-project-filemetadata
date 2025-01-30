f5b36b9b49e26c7468d7cdd03e706fdbd8f3eaf6
hamza-coding-usthb taourirt <taourirt.hamza.etu@gmail.com>
2025-01-30 01:46:03 +0000

    finished challenge five

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+const fs = require("fs");
+const path = require("path");
 
 var app = express();
 
+const uploadDir = path.join(__dirname, "uploads");
+
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+const upload = multer({ dest: uploadDir });
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype, 
+    size: req.file.size, 
+  });
 });
 
 
